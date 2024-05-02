0544b3e06b6077ada59bfd8c43fe8ed1b502e293
Danish Tran <danishdtran@gmail.com>
2024-05-01 18:42:07 -0700

    Home Page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React from 'react';
+import HomePage from './components/homeComponents/HomePage';
 // import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <div 
-        style={{
-          display: 'flex', 
-          justifyContent: 'center', 
-          alignItems: 'center',
-        }}>
-        <button>1</button>
-        <button>1</button>
-        <button>1</button>
-        <button>1</button>
-        <button>1</button>
-        <button>1</button>
-      </div>
+      <HomePage />
     </div>
   );
 }
--- src/components/homeComponents/HomePage.js
+++ src/components/homeComponents/HomePage.js
@@ -1,9 +1,65 @@
 import React from 'react';
 
 function HomePage() {
+  const buttonStyle = {
+    borderRadius: '40px',
+    height: '10%',
+    width: '40%',
+    margin: '1vh',
+    fontFamily: 'Anonymous Pro',
+    fontSize: '1vw',
+    background: '#ADADAD',
+    border: 'none',
+  }
+
   return (
-    <div>
-      temp
+    <div
+      style={{
+      height: '80vh',
+      width: '99vw',
+      background: '#FFFCF7',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }}>
+      <div 
+        style={{
+            height: '80%',
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+        <span 
+          style={{
+            height: '100%',
+            width: '30%',
+            background: '#FFF8EB',
+            display: 'flex', 
+            justifyContent: 'center', 
+            alignItems: 'center',
+            flexDirection: 'column',
+            fontFamily: 'Anonymous Pro',
+          }}
+        >
+          <p 
+            style={{
+              fontWeight: 'bold',
+              fontSize: '3vw',
+            }}
+          >
+            Welcome
+          </p>
+
+          <button style={buttonStyle}>Set/Rep Saver</button>
+          <button style={buttonStyle}>Macro Saver</button>
+          <button style={buttonStyle}>Weight Saver</button>
+          <button style={buttonStyle}>Discover Workouts</button>
+          <button style={buttonStyle}>Login</button>
+          <button style={buttonStyle}>Profile</button>
+        </span>
+      </div>
     </div>
   );
 }
