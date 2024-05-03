af3953a4745357b9aad687d83b4bd3d76c01e287
Alvin Wu <alvinw9@uci.edu>
2024-05-03 01:13:31 -0700

    Origin page completed

--- src/components/BarComponent/Bar.js
+++ src/components/BarComponent/Bar.js
@@ -1,41 +1,55 @@
 import {React, useState} from 'react';
 
+const dropdownitems = {
+    padding: '2%'
+}
+
 function Bar() {
     const [dropDownToggle, setDropDownToggle] = useState(false);
 
 
     return (
-        <div style={{width: '100%'}}>
-            <div style={{height: "20vh", width:"100%", backgroundColor: "#022F40", alignItems: 'center', display:'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
-                
-                
-                <div style={{fontSize: '4vw', color: 'white', padding: '10px'}}>Ready Set Result</div>
+        <div>
+            <div style={{width: '100vw', marginLeft: '-1vw', marginTop: '-1.5vh'}}>
+                <div 
+                    style={{
+                        height: "15vh", 
+                        backgroundColor: "#022F40", 
+                        alignItems: 'center', 
+                        display:'flex', 
+                        flexDirection: 'row', 
+                        justifyContent: 'space-between'
+                    }}>
+                    
+                    
+                    <div style={{fontSize: '4vw', color: 'white', marginLeft: '2vw'}}>Ready Set Result</div>
 
-                <div style={{padding:'10px'}}>
-                    <button onClick={() => setDropDownToggle((prev) => (!prev))} style={{backgroundColor: 'transparent', color: 'white', border: 'none', fontSize: '1vw'}}>
-                        <p>Menu</p>
-                        <p>----------------</p>
-                        <p>----------------</p>
-                        <p>----------------</p>
-                    </button>
+                    <div style={{marginTop:'1vh', marginRight: '2vw'}}>
+                        <button onClick={() => setDropDownToggle((prev) => (!prev))} style={{backgroundColor: 'transparent', color: 'white', border: 'none', fontSize: '1.5vh'}}>
+                            <p>Menu</p>
+                            <p>----------------</p>
+                            <p>----------------</p>
+                            <p>----------------</p>
+                        </button>
+                    </div>
                 </div>
-            </div>
-            
-            <div style={{display: 'block', position:'absolute', zIndex: '1', right:'0'}}>
-                <div style={{width: '25vh', backgroundColor: '#FFF8EB', boxShadow: '0px 8px 12px 0px rgba(0,0,0,0.2)', textAlign: 'left'}}>
-                    {dropDownToggle &&  
-                        <div style={{fontSize: '2vw'}}>
-                            <div style={{padding: '10px'}}>Set/Rep Saver</div>
-                            <div style={{padding: '10px'}}>Macro Saver</div>
-                            <div style={{padding: '10px'}}>Weight Saver</div>
-                            <div style={{padding: '10px'}}>Discover Workouts</div>
-                            <div style={{padding: '10px'}}>Login</div>
-                            <div style={{padding: '10px'}}>Profile</div>
-                        </div>
-                    }
+                
+                <div style={{position:'absolute', zIndex: '1', right:'2vh'}}>
+                    <div style={{backgroundColor: '#FFF8EB', boxShadow: '0px 8px 12px rgba(0,0,0,0.2)', textAlign: 'left'}}>
+                        {dropDownToggle &&  
+                            <div style={{fontSize: '2vw'}}>
+                                <div style={dropdownitems}>Set/Rep Saver</div>
+                                <div style={dropdownitems}>Macro Saver</div>
+                                <div style={dropdownitems}>Weight Saver</div>
+                                <div style={dropdownitems}>Discover Workouts</div>
+                                <div style={dropdownitems}>Login</div>
+                                <div style={dropdownitems}>Profile</div>
+                            </div>
+                        }
+                    </div>
                 </div>
-            </div>
 
+            </div>
         </div>
     )
 }
