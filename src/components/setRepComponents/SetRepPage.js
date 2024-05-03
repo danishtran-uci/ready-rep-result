0d2e9a883e0034f628f375fbd56a5924bfd58953
Alvin Wu <alvinw9@uci.edu>
2024-05-02 22:09:16 -0700

    Working on button functionality

--- src/components/setRepComponents/SetRepPage.js
+++ src/components/setRepComponents/SetRepPage.js
@@ -1,11 +1,46 @@
-import {React} from 'react';
+import {React, useState} from 'react';
 
 function SetRepPage() {
+
+  // const [exercises, addExercise] = useState({"Shoulder Press": [20,8,20,8,20,8]});
+  // const [sets, addSets] = useState('': []);
+
+  // function addExToExercises()
+  // {
+  //   sets[0] = "Bench Press";
+  //   var canAdd = true;
+  //   if (sets[1].length !== 6)
+  //     {
+  //       canAdd = false;
+  //     }
+  //   for (let set of sets[1])
+  //   {
+  //     if (set < 0)
+  //     {
+  //       canAdd = false;
+  //     }
+  //   }
+
+  //   if (canAdd)
+  //     {
+  //       console.log('a');
+  //       console.log(sets);
+  //       addExercise((preqList) => ({
+  //         ...preqList, 
+  //         [sets[0]]: sets[1]
+  //       }));
+  //       console.log(exercises);
+  //     }
+  //   else
+  //   {
+  //     alert('Please enter valid inputs');
+  //   }
+  // }
 
   const buttonStyle = {
     borderRadius: '40px',
     margin: '1vh',
-    fontSize: '2.5vw',
+    fontSize: '2vw',
     fontFamily: 'Anonymous Pro',
     background: '#ADADAD',
     border: 'none',
@@ -28,7 +63,7 @@ function SetRepPage() {
     <div>
       <div style={{width: "100%", height: "80vh", alignItems: 'center', display:'flex', flexDirection: 'row'}}>
         <div style={{width: "50%", backgroundColor: '#FFF8EB', border: "5px solid white"}}>
-          <div style={{height: '25%', fontSize: '5vw'}}>
+          <div style={{height: '25%', fontSize: '3vw'}}>
             <div style={{padding: '10px'}}>
               <div style={{float: "right"}}>
                   <button style={buttonStyle}>Change Workout</button>
@@ -37,7 +72,7 @@ function SetRepPage() {
             </div>
           </div>
 
-          <div style={{textAlign: "center", height: '50%', fontSize: '5vw'}}>
+          <div style={{textAlign: "center", height: '50%', fontSize: '3vw'}}>
             <div>
               Set 1 - <input type="number" style={inputStyle}></input>lbs: <input type="number" style={inputStyle}></input> Rep
             </div>
@@ -61,11 +96,11 @@ function SetRepPage() {
         </div>
         
         <div style={{width: "50%", backgroundColor: '#FFF8EB', border: "5px solid white"}}>
-          <div style={{height: "25%", fontSize: '5vw'}}>
+          <div style={{height: "25%", fontSize: '3vw'}}>
             <div style={{padding: '10px'}}>Current Workout</div>
           </div>
 
-          <div style={{textAlign: "center", height: '50%', fontSize: '5vw'}}>
+          <div style={{textAlign: "center", height: '50%', fontSize: '3vw'}}>
             <div>Shoulder Press</div>
             <div>
               Set 1 - (20 lb): 8 rep 
