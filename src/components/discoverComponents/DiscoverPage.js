c96a236b3bf7d3fdd3f1cba1fc911ac6ecdc7b28
Alvin Wu <alvinw9@uci.edu>
2024-05-17 22:12:16 -0700

    Completed API searches

--- src/components/discoverComponents/DiscoverPage.js
+++ src/components/discoverComponents/DiscoverPage.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import {React, useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import Bar from '../BarComponent/Bar';
+import { Box, Button, TextField, Typography} from '@mui/material';
+import {exerciseOptions, fetchData} from '../discoverComponents/fetchData';
 
-const buttonStyle = {
-  borderRadius: '40px',
-  width: '80%',
-  fontFamily: 'Anonymous Pro',
-  fontSize: '1vw',
-  background: '#ADADAD',
-  border: 'none',
-}
+// const buttonStyle = {
+//   borderRadius: '40px',
+//   width: '80%',
+//   fontFamily: 'Anonymous Pro',
+//   fontSize: '1vw',
+//   background: '#ADADAD',
+//   border: 'none',
+// }
 
 const backgroundStyle = {
   height: '100%',
@@ -19,32 +21,44 @@ const backgroundStyle = {
   margin: '5vw',
 }
 
-const workoutRowStyle = {
-  height: '40%',
-  marginTop: '-4vh',
-  marginBottom: '1vh',
-  marginLeft: '1vw',
-  marginRight: '1vw',
-  display: 'flex',
-  justifyContent: 'space-evenly',
-}
+// const workoutRowStyle = {
+//   height: '40%',
+//   marginTop: '-4vh',
+//   marginBottom: '1vh',
+//   marginLeft: '1vw',
+//   marginRight: '1vw',
+//   display: 'flex',
+//   justifyContent: 'space-evenly',
+// }
 
-const workoutSectionStyle = {
-  width: '33%',
-  display: 'flex',
-  justifyContent: 'center',
-}
+// const workoutSectionStyle = {
+//   width: '33%',
+//   display: 'flex',
+//   justifyContent: 'center',
+// }
 
-const workoutContentStyle = {
-  flexDirection: 'column',
-  width: '100%',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'space-evenly',
-}
+// const workoutContentStyle = {
+//   flexDirection: 'column',
+//   width: '100%',
+//   display: 'flex',
+//   alignItems: 'center',
+//   justifyContent: 'space-evenly',
+// }
 
 function DiscoverPage() {
-  const navigate = useNavigate();
+
+  const [search, setSearch] = useState('');
+  const [exercises, setEx] = useState([]);
+
+  const handleSearch = async () => {
+    if (search) {
+      const exerciseData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/name/${search}`, exerciseOptions);
+      console.log(exerciseData);
+    
+      setEx(exerciseData);
+      setSearch('');
+    }
+  };
 
   return (
     <div>
@@ -84,191 +98,34 @@ function DiscoverPage() {
             >
               Discover
             </p>
-
-            <div style={workoutRowStyle}>
-              <div style={workoutSectionStyle}>
-                <div style={workoutContentStyle}>
-                  <div style={{ fontSize: '1.5vw' }}>Chest</div>
-                  <button
-                    style={buttonStyle}
-                    onClick={() => (
-                      navigate("/ExerciseInfo", { state: 'Bench Press' })
-                    )}
-                  >
-                    Bench Press
-                  </button>
-                  <button
-                    style={buttonStyle}
-                    onClick={() => (
-                      navigate("/ExerciseInfo", { state: 'Incline Press' })
-                    )}
-                  >
-                    Incline Press
-                  </button>
-                  <button
-                    style={buttonStyle}
-                    onClick={() => (
-                      navigate("/ExerciseInfo", { state: 'Cable Crossover' })
-                    )}
-                  >
-                    Cable Crossover
-                  </button>
+            <div>
+              <Box position="relative" mb="72px">
+                <TextField
+                  height="76px"
+                  sx={{ input: { fontWeight: '700', border: 'none', borderRadius: '4px' }, width: { lg: '1170px', xs: '350px' }, backgroundColor: '#fff', borderRadius: '40px' }}
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value.toLowerCase())}
+                  placeholder="Search Exercises"
+                  type="text"
+                />
+                <Button className="search-btn" sx={{ bgcolor: '#FF2625', color: '#fff', textTransform: 'none', width: { lg: '173px', xs: '80px' }, height: '56px', position: 'absolute', right: '0px', fontSize: { lg: '20px', xs: '14px' } }} onClick={handleSearch}>
+                  Search
+                </Button>
+              </Box>
+            </div>
+            
+            <div style={{display: 'flex',
+                justifyContent: 'center', flexWrap:"wrap"}}>
+              {exercises.map((entry, index) => (
+                <div>
+                  <img src={entry.gifUrl}></img>
+                  <Typography>
+                    {entry.name}
+                  </Typography>
                 </div>
-              </div>
-              <div style={workoutSectionStyle}>
-                <div style={workoutContentStyle}>
-                  <div style={{ fontSize: '1.5vw' }}>Back</div>
-                  <button
-                    style={buttonStyle}
-                    onClick={() => (
-                      navigate("/ExerciseInfo", { state: 'Deadlift' })
-                    )}
-                  >
-                    Deadlift
-                  </button>
-                  <button
-                    style={buttonStyle}
-                    onClick={() => (
-                      navigate("/ExerciseInfo", { state: 'Dumbbell Rows' })
-                    )}
-                  >
-                    Dumbbell Rows
-                  </button>
-                  <button
-                    style={buttonStyle}
-                    onClick={() => (
-                      navigate("/ExerciseInfo", { state: 'Lat Pulldown' })
-                    )}
-                  >
-                    Lat Pulldown
-                  </button>
-                </div>
-              </div>
-              <div style={workoutSectionStyle}>
-                <div style={workoutContentStyle}>
-                  <div style={{ fontSize: '1.5vw' }}>Arms</div>
-                  <button
-                    style={buttonStyle}
-                    onClick={() => (
-                      navigate("/ExerciseInfo", { state: 'Bicep Curls' })
-                    )}
-                  >
-                    Bicep Curls
-                  </button>
-                  <button
-                    style={buttonStyle}
-                    onClick={() => (
-                      navigate("/ExerciseInfo", { state: 'Cable Tricep Pushdown' })
-                    )}
-                  >
-                    Cable Tricep Pushdown
-                  </button>
-                  <button
-                    style={buttonStyle}
-                    onClick={() => (
-                      navigate("/ExerciseInfo", { state: 'Skull Crushers' })
-                    )}
-                  >
-                    Skull Crushers
-                  </button>
-                </div>
-              </div>
+              ))}
             </div>
 
-            <div
-              style={{
-                ...workoutRowStyle,
-                marginTop: '0vh',
-              }}
-            >
-              <div style={workoutSectionStyle}>
-                <div style={workoutContentStyle}>
-                  <div style={{ fontSize: '1.5vw' }}>Legs</div>
-                  <button
-                    style={buttonStyle}
-                    onClick={() => (
-                      navigate("/ExerciseInfo", { state: 'Squats' })
-                    )}
-                  >
-                    Squats
-                  </button>
-                  <button
-                    style={buttonStyle}
-                    onClick={() => (
-                      navigate("/ExerciseInfo", { state: 'Leg Extension' })
-                    )}
-                  >
-                    Leg Extension
-                  </button>
-                  <button
-                    style={buttonStyle}
-                    onClick={() => (
-                      navigate("/ExerciseInfo", { state: 'Hamstring Curls' })
-                    )}
-                  >
-                    Hamstring Curls
-                  </button>
-                </div>
-              </div>
-              <div style={workoutSectionStyle}>
-                <div style={workoutContentStyle}>
-                  <div style={{ fontSize: '1.5vw' }}>Shoulder</div>
-                  <button
-                    style={buttonStyle}
-                    onClick={() => (
-                      navigate("/ExerciseInfo", { state: 'Overhead Press' })
-                    )}
-                  >
-                    Overhead Press
-                  </button>
-                  <button
-                    style={buttonStyle}
-                    onClick={() => (
-                      navigate("/ExerciseInfo", { state: 'Side Lateral Raise' })
-                    )}
-                  >
-                    Side Lateral Raise
-                  </button>
-                  <button
-                    style={buttonStyle}
-                    onClick={() => (
-                      navigate("/ExerciseInfo", { state: 'Front Raise' })
-                    )}
-                  >
-                    Front Raise
-                  </button>
-                </div>
-              </div>
-              <div style={workoutSectionStyle}>
-                <div style={workoutContentStyle}>
-                  <div style={{ fontSize: '1.5vw' }}>Core</div>
-                  <button
-                    style={buttonStyle}
-                    onClick={() => (
-                      navigate("/ExerciseInfo", { state: 'Plank' })
-                    )}
-                  >
-                    Plank
-                  </button>
-                  <button
-                    style={buttonStyle}
-                    onClick={() => (
-                      navigate("/ExerciseInfo", { state: 'Crunch' })
-                    )}
-                  >
-                    Crunch
-                  </button>
-                  <button
-                    style={buttonStyle}
-                    onClick={() => (
-                      navigate("/ExerciseInfo", { state: 'Side Plank' })
-                    )}
-                  >
-                    Side Plank
-                  </button>
-                </div>
-              </div>
-            </div>
           </span>
         </div>
       </div>
